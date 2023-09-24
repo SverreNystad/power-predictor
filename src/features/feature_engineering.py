198d162b33a53608da7184773204b7bb12bca571
Sverre Nystad <sverrenystad@gmail.com>
2023-09-24 13:51:04 +0200

    Feat: Add prepare data

--- src/features/feature_engineering.py
+++ src/features/feature_engineering.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 import pandas as pd
-
+from typing import Tuple
 
 def create_time_features_from_date(data_frame: pd.DataFrame) -> pd.DataFrame:
     """
@@ -67,3 +67,76 @@ def remove_date_time_feature(data_frame: pd.DataFrame) -> pd.DataFrame:
     df.drop(["date_forecast"], axis=1, inplace=True)
     return df
 
+
+def clean_data(data_frame: pd.DataFrame) -> pd.DataFrame:
+    """
+    Clean data frame by removing outliers and NaN values.
+    
+    Args:
+        df (pd.DataFrame): Data frame with date_forecast column.
+    Returns:
+        pd.DataFrame: Data frame copy without outliers and NaN values.
+
+    """
+    df = data_frame.copy()
+    df = create_time_features_from_date(df)
+    df = df.dropna()
+    df = df[df["target"] > 0]
+    return df
+
+
+def prepare_data(targets: pd.DataFrame, 
+                 features_observed: pd.DataFrame, 
+                 features_estimated: pd.DataFrame, 
+                 features_test: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    """
+    Prepare data for modeling.
+    
+    Args:
+        targets (pd.DataFrame): Data frame with target column.
+        features_observed (pd.DataFrame): Data frame with observed features.
+        features_estimated (pd.DataFrame): Data frame with estimated features.
+        features_test (pd.DataFrame): Data frame with test features.
+    
+    Returns:
+    Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]: 
+        Tuple containing X_train, y_train, X_val, y_val, X_test, train_data, and val_data DataFrames.
+    """
+
+    # Convert the date column to datetime type
+    targets['time'] = pd.to_datetime(targets['time'])
+    features_observed['date_forecast'] = pd.to_datetime(features_observed['date_forecast'])
+    features_estimated['date_forecast'] = pd.to_datetime(features_estimated['date_forecast'])
+    features_test['date_forecast'] = pd.to_datetime(features_test['date_forecast'])
+
+    #  Set the Date Column as Index
+    targets.set_index('time', inplace=True)
+    features_observed.set_index('date_forecast', inplace=True)
+    features_estimated.set_index('date_forecast', inplace=True)
+    features_test.set_index('date_forecast', inplace=True)
+
+    # Drop rows with any missing values in targets
+    targets.dropna(inplace=True)
+    
+    # Downsampling the features to hourly resolution by taking the mean of every 4 rows
+    features_observed = features_observed.resample('1H').mean()
+    features_estimated = features_estimated.resample('1H').mean()
+    features_test = features_test.resample('1H').mean()
+    
+    # Merge observed features and targets for training
+    train_data = pd.concat([targets, features_observed], axis=1, join='inner').dropna()
+    
+    # Merge estimated features and targets for validation
+    val_data = pd.concat([targets, features_estimated], axis=1, join='inner').dropna()
+    
+    # Define X_train, y_train, X_val, y_val
+    X_train = train_data.drop(columns=['pv_measurement'])
+    y_train = train_data['pv_measurement']
+    
+    X_val = val_data.drop(columns=['pv_measurement'])
+    y_val = val_data['pv_measurement']
+    
+    # For test data, you only have features
+    X_test = features_test
+    
+    return X_train, y_train, X_val, y_val, X_test, train_data, val_data
