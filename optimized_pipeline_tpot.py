11e56a2379cee6977fcd5fdde1a3caa66ec8ffd9
Sverre Nystad <sverrenystad@gmail.com>
2023-09-29 16:37:13 +0200

    Feat: Add automl with all data

--- optimized_pipeline_tpot.py
+++ optimized_pipeline_tpot.py
@@ -1,27 +1,19 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import ExtraTreesRegressor
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline, make_union
-from sklearn.preprocessing import StandardScaler
-from tpot.builtins import StackingEstimator
 from xgboost import XGBRegressor
-from sklearn.metrics import mean_absolute_error
-from tpot.export_utils import set_param_recursive
 
+# NOTE: Make sure that the outcome column is labeled 'target' in the data file
+tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
+features = tpot_data.drop('target', axis=1)
+training_features, testing_features, training_target, testing_target = \
+            train_test_split(features, tpot_data['target'], random_state=42)
 
-X_train, y_train, X_val, y_val = fetch_preprocessed_uniform_data()
+# Average CV score on the training set was: -86.3409718614859
+exported_pipeline = XGBRegressor(learning_rate=0.1, max_depth=9, min_child_weight=6, n_estimators=100, n_jobs=1, objective="reg:squarederror", subsample=1.0, verbosity=0)
+# Fix random state in exported estimator
+if hasattr(exported_pipeline, 'random_state'):
+    setattr(exported_pipeline, 'random_state', 42)
 
-# Average CV score on the training set was: -15.83693161088147
-exported_pipeline = make_pipeline(
-    StackingEstimator(estimator=XGBRegressor(learning_rate=0.001, max_depth=9, min_child_weight=16, n_estimators=100, n_jobs=1, objective="reg:squarederror", subsample=0.6500000000000001, verbosity=0)),
-    StandardScaler(),
-    ExtraTreesRegressor(bootstrap=False, max_features=0.6500000000000001, min_samples_leaf=5, min_samples_split=16, n_estimators=100)
-)
-# Fix random state for all the steps in exported pipeline
-set_param_recursive(exported_pipeline.steps, 'random_state', 42)
-
-exported_pipeline.fit(X_train, y_train)
-results = exported_pipeline.predict(X_val)
-
-print(f"MAE: {mean_absolute_error(y_val, results)}")
+exported_pipeline.fit(training_features, training_target)
+results = exported_pipeline.predict(testing_features)
