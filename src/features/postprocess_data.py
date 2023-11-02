1ffa35d86b18e9aa47ade53b3a5f9f1044896315
Sverre Nystad <sverrenystad@gmail.com>
2023-11-02 18:12:36 +0100

    Feat: Add postprocess_data

--- src/features/postprocess_data.py
+++ src/features/postprocess_data.py
@@ -1,16 +1,17 @@
 import pandas as pd
 import math
 
-# Post processing:
-# location A - 0 pv kl 23
-# location C - 0 pv kl 22,,23,00
-TIMES_TO_SET_0_PV_A = [23]
-TIMES_TO_SET_0_PV_C = [22, 23, 0]
 find_time_sin = lambda hour: math.sin(2 * math.pi * (hour) / 24)
 find_time_cos = lambda hour: math.cos(2 * math.pi * (hour) / 24)
 
+def postprocess_data(x_test: pd.DataFrame, y_pred: pd.DataFrame) -> pd.DataFrame:
+    """Postprocess the data to set the predicted values to 0 at the correct times."""
+    
+    # Set the predicted values to 0 at the correct times
+    y_pred = set_0_pv_at_times(x_test, y_pred, "a", [22, 23, 0])
+    y_pred = set_0_pv_at_times(x_test, y_pred, "c", [23])
 
-
+    return y_pred
 
 def set_0_pv_at_times(x_test: pd.DataFrame, y_pred: pd.DataFrame, location: str, hours: list[int]) -> pd.DataFrame:
     """Find the correct predicted values at the given times and locaiton and set them to 0."""
