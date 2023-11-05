8d061ed707326a28adecb977df20f53c467c3f27
Sverre Nystad <sverrenystad@gmail.com>
2023-11-05 15:43:24 +0100

    Feat: Add lines to separate location a b and c

--- src/visualization/plotting.py
+++ src/visualization/plotting.py
@@ -492,10 +492,20 @@ def plot_residual_predictions(submission_1_name: str, submission_2_name: str) ->
     prediction_1 = pd.read_csv(PREDICTION_PATH + submission_1_name + ".csv")
     prediction_2 = pd.read_csv(PREDICTION_PATH + submission_2_name + ".csv")
     residual = prediction_1 - prediction_2
+    amount_of_predictions = residual.shape[0]
+    # Create lines two separate location A, B and C By taking 1/3 of the total amount of predictions as index line
+    line_a_b = 1/3 * amount_of_predictions
+    line_b_c = 2/3 * amount_of_predictions
+    
     # Plot the residuals
     plt.figure(figsize=(12, 6))
     # Line plot of Residuals
     plt.plot(residual, label='Residuals', linestyle='-', marker='o', markersize=5, alpha=0.7, color='blue')
+    
+    # Create lines to separate location A, B and C
+    plt.axvline(x=line_a_b, color='black', linestyle='--')
+    plt.axvline(x=line_b_c, color='black', linestyle='--')
+    
     # Titles and labels
     plt.title(f'Residuals: {submission_1_name} - {submission_2_name}', fontsize=16)
     plt.ylabel("Pv Measurement ")
