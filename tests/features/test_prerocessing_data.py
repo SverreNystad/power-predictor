95a3ee8e4e614d49a2474e9101e4ebf6a6bd6bbf
Sverre Nystad <sverrenystad@gmail.com>
2023-11-05 20:46:11 +0100

    Fix: Correct SUBMISSION_LENGTH to requirement
    Co-authored-by: Gunnar Nystad <nGunnar@hotmail.no>

--- tests/features/test_prerocessing_data.py
+++ tests/features/test_prerocessing_data.py
@@ -68,7 +68,7 @@ def test_get_preprocessed_test_data_has_no_missing_values():
 def test_get_preprocessed_test_data_has_same_amount_of_rows_as_final_test():
     # Arrange
     x_test_whole = get_preprocessed_test_data()
-    SUBMISSION_LENGTH = 2660
+    SUBMISSION_LENGTH = 2160
     
     # Check that the test data has the same number of rows as the final test data
     assert x_test_whole.shape[0] == SUBMISSION_LENGTH
