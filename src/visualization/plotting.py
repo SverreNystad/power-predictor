3b47464258a36d2759060d5bc41ba2165413263c
Sverre Nystad <sverrenystad@gmail.com>
2023-11-05 15:29:09 +0100

    Feat: Add residual plot
    Co-authored-by: Gunnar Nystad <nGunnar@hotmail.no>

--- src/visualization/plotting.py
+++ src/visualization/plotting.py
@@ -482,3 +482,27 @@ def plot_actual_vs_prediction(actual, prediction) -> None:
     plt.tight_layout()
     plt.savefig(f"{FIGURE_PATH}prediction_vs_actuals/actual_vs_predicted.png")
     plt.show()
+
+
+PREDICTION_PATH = "results/output/"
+def plot_residual_predictions(submission_1_name: str, submission_2_name: str) -> None:
+    """
+    Plot the residuals of two submissions. The residuals are the difference between the two submissions.
+    """
+    prediction_1 = pd.read_csv(PREDICTION_PATH + submission_1_name + ".csv")
+    prediction_2 = pd.read_csv(PREDICTION_PATH + submission_2_name + ".csv")
+    residual = prediction_1 - prediction_2
+    # Plot the residuals
+    plt.figure(figsize=(12, 6))
+    # Line plot of Residuals
+    plt.plot(residual, label='Residuals', linestyle='-', marker='o', markersize=5, alpha=0.7, color='blue')
+    # Titles and labels
+    plt.title(f'Residuals: {submission_1_name} - {submission_2_name}', fontsize=16)
+    plt.ylabel("Pv Measurement ")
+    plt.xlabel("Time")
+    plt.legend()
+    
+    plt.grid(True)
+    plt.tight_layout()
+    plt.savefig(f"{FIGURE_PATH}residuals/{submission_1_name}_vs_{submission_2_name}.png")
+    plt.show()
